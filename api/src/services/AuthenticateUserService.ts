f3185c2c1cdb19bf836243bccfc7a578a7ddda8f
Kevin Kuhn <kmulinarik@hotmail.com>
2021-10-27 23:26:16 -0300

    feat messages model

--- api/src/services/AuthenticateUserService.ts
+++ api/src/services/AuthenticateUserService.ts
@@ -8,6 +8,8 @@
  * Retorna o token com as infos do usuario
  */
 import axios from "axios"
+import prismaClient from "../prisma"
+import { sign } from "jsonwebtoken"
 
 interface IAccessTokenResponse {
 	access_token: string
@@ -24,24 +26,65 @@ class AuthenticateUserService {
 	async execute(code: string) {
 		const url = "https://github.com/login/oauth/access_token"
 
-		const { data: accesTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
-			params: {
-				client_id: process.env.GITHUB_CLIENT_ID,
-				client_secret: process.env.GITHUB_CLIENT_SECRET,
-				code
+		const { data: accesTokenResponse } = await axios.post<IAccessTokenResponse>(
+			url,
+			null,
+			{
+				params: {
+					client_id: process.env.GITHUB_CLIENT_ID,
+					client_secret: process.env.GITHUB_CLIENT_SECRET,
+					code,
+				},
+				headers: {
+					Accept: "application/json",
+				},
+			}
+		)
+
+		const response = await axios.get<IUserResponse>(
+			"https://api.github.com/user",
+			{
+				headers: {
+					authorization: `Bearer ${accesTokenResponse.access_token}`,
+				},
+			}
+		)
+
+		const { login, id, avatar_url, name } = response.data
+
+		let user = await prismaClient.user.findFirst({
+			where: {
+				github_id: id,
 			},
-			headers: {
-				Accept: "application/json"
-			}
 		})
 
-		const response = await axios.get<IUserResponse>("https://api.github.com/user", {
-			headers: {
-				authorization: `Bearer ${accesTokenResponse.access_token}`
-			}
-		})
+		if (!user) {
+			user = await prismaClient.user.create({
+				data: {
+					github_id: id,
+					login,
+					avatar_url,
+					name,
+				},
+			})
+		}
 
-		return response.data
+		const token = sign(
+			{
+				user: {
+					name: user.name,
+					avatar_url: user.avatar_url,
+					id: user.id,
+				},
+			},
+			process.env.JWT_SECRET,
+			{
+				subject: user.id,
+				expiresIn: "1d",
+			}
+		)
+
+		return { user, token }
 	}
 }
 
