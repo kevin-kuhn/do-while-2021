8014a6b971044e8b19b8ef091728843291230caf
Kevin Kuhn <kmulinarik@hotmail.com>
2021-11-02 16:45:38 -0300

    initial commmit web app nlw

---
+++ web-app/src/App.tsx
@@ -0,0 +1,13 @@
+
+import styles from './App.module.css'
+
+const App = () => {
+
+  return (
+    <main className={styles.contentWrapper}>
+			<h1>Hello World</h1>
+		</main>
+  )
+}
+
+export default App
